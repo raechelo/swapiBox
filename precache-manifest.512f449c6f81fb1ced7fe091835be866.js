90a8a1a6aa2066524e222af5bce9776a51c9de75
raechel <odom.raechel@gmail.com>
2019-07-11 15:17:06 -0600

    Updates

---
+++ precache-manifest.512f449c6f81fb1ced7fe091835be866.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1cffd579e47dac6a409efb16ee41dd",
+    "url": "/swapibox/index.html"
+  },
+  {
+    "revision": "af8df73f5b135bb6ff2d",
+    "url": "/swapibox/static/css/2.d1f26340.chunk.css"
+  },
+  {
+    "revision": "5a6738d2ac7b83ef8ef0",
+    "url": "/swapibox/static/css/main.1809972b.chunk.css"
+  },
+  {
+    "revision": "af8df73f5b135bb6ff2d",
+    "url": "/swapibox/static/js/2.68bc95e4.chunk.js"
+  },
+  {
+    "revision": "5a6738d2ac7b83ef8ef0",
+    "url": "/swapibox/static/js/main.6096c42f.chunk.js"
+  },
+  {
+    "revision": "58a9faa0586694503f05",
+    "url": "/swapibox/static/js/runtime~main.df4fd0ab.js"
+  },
+  {
+    "revision": "b68da5e8710c4abe30a0838ec2770e0f",
+    "url": "/swapibox/static/media/SithEmblem.b68da5e8.png"
+  }
+]);
